9424c179f2db401dfd9ab7be6304591cfa602f56
jonatasjs <soaresjonatas398@gmail.com>
2022-10-30 19:55:22 -0300

    zxczxc

--- pages/teste/index.tsx
+++ pages/teste/index.tsx
@@ -2,17 +2,17 @@ import Head from "next/head";
 
 export default function Teste() {
   return (
-    <main>
+    <>
       <Head>
         <title>Test Page</title>
       </Head>
       <h1>TESTE</h1>
-    </main>
+    </>
   );
 }
 
 export async function getStaticProps() {
   return {
     props: { post: {} },
-  };
+  }
 }

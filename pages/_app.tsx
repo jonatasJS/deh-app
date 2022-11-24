8399702d99a523cb2f4a96bcaff9c19f0d2613f7
jonatasJS <soaresjonatas398@gmail.com>
2022-11-24 13:55:59 -0300

    sdfsdfd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import { useRouter } from "next/router";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 import Sidebar from "../components/Sidebar";
 
@@ -26,20 +26,22 @@ export default function App({ Component, pageProps }: AppProps) {
         src="https://sim.tins.com.br/media/1/usuarios/60911012346.jpg?externo=true"
         alt="Debora"
       />
-      <motion.main
-        key={router.asPath}
-        variants={{
-          hidden: { opacity: 0, x: -200, y: 0 },
-          enter: { opacity: 1, x: 0, y: 0 },
-          exit: { opacity: 0, x: 0, y: -200 },
-        }}
-        initial="hidden"
-        animate="enter"
-        exit="exit"
-        transition={{ type: "linear" }}
-      >
-        <Component {...pageProps} />
-      </motion.main>
+      <AnimatePresence exitBeforeEnter>
+        <motion.main
+          key={router.asPath}
+          variants={{
+            hidden: { opacity: 0, x: -500 },
+            enter: { opacity: 1, x: 0 },
+            exit: { opacity: 0, y: -500 },
+          }}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          transition={{ type: "linear" }}
+        >
+          <Component {...pageProps} />
+        </motion.main>
+      </AnimatePresence>
       <ToastContainer />
     </div>
   );
